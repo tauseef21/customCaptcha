69d1c2f91ad6743600408b00d9c1643c08fbaff7
tauseef <tauseefahmed.shaikh@astoriasolutions.com>
2021-02-11 12:24:29 +0530

    custom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'customCaptcha';
+export class AppComponent implements OnInit {
+
+  title = 'custom-captcha';
+  captcha: number;
+  _show: boolean = false;
+
+  ngOnInit(): void {
+    this.refreshCaptcha();
+  }
+  refresh() {
+    this.refreshCaptcha();
+  }
+  validate() {
+    let val = ((document.getElementById("incaptcha") as HTMLInputElement).value);
+    if (val == this.captcha.toString()) {
+      alert("login successful");
+      ((document.getElementById("incaptcha") as HTMLInputElement).value) = "";
+      this.refreshCaptcha();
+      this._show = false;
+    } else {
+      alert("please enter valid captcha");
+      this.refreshCaptcha();
+    }
+  }
+  showCaptcha() {
+    this._show = true;
+  }
+  refreshCaptcha() {
+    this.captcha = Math.floor(Math.random() * 1001232) + 1;
+  }
 }
